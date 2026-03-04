0dfad54e791eb74ac1f48159486aed6aa6518329
zenin1504 <kayozzprojects@yandex.com>
2026-03-04 15:51:48 +0800

    ota: Allow unofficial build to use updater
    Change-Id: I20b511abc4954b39845449ce097f329e
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- config/ota.mk
+++ config/ota.mk
@@ -1,9 +1,6 @@
 # Updater
-ifeq ($(IS_OFFICIAL),true)
 PRODUCT_PACKAGES += \
     Updater
 
 PRODUCT_COPY_FILES += \
     vendor/custom/config/permissions/privapp-permissions-custom.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-custom.xml
-
-endif
