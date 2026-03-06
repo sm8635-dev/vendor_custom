62201f7ad9689064f96e2568ccc444dc7a339b44
zenin1504 <kayozzprojects@yandex.com>
2026-03-06 16:43:26 +0800

    ota: Allow to use updater
    Change-Id: I9c0806454e4f41a3ae828ccb9d8daa4c
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- config/version.mk
+++ config/version.mk
@@ -12,8 +12,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.custom.version=PixelOS_$(CUSTOM_VERSION)
 
 # Updater
-ifeq ($(IS_OFFICIAL),true)
-    PRODUCT_PRODUCT_PROPERTIES += \
-        net.pixelos.build_type=ci \
-        net.pixelos.version=$(CUSTOM_VERSION_PROP)
-endif
+PRODUCT_PRODUCT_PROPERTIES += \
+    net.pixelos.build_type=ci \
+    net.pixelos.version=$(CUSTOM_VERSION_PROP)
